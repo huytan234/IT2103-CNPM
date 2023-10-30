abaab203be0875363bcd330d8445eea5413f34b8
unknown <>
2023-10-30 11:51:36 +0700

    init project sesion 1

---
+++ saleappv1/app/dao.py
@@ -0,0 +1,30 @@
+def get_categories():
+    return [{
+        'id': 1,
+        'name': 'Mobile'
+    }, {
+        'id': 2,
+        'name': 'Tablet'
+    }]
+
+
+def get_products():
+    return [{
+        'id': 1,
+        'name': 'Iphone 15 pro max',
+        'price': 25000000,
+        'image': "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-naturaltitanium?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692845702708",
+        'categories_id': 1
+    }, {
+        'id': 2,
+        'name': 'Iphone 15 pro max',
+        'price': 25000000,
+        'image': "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-naturaltitanium?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692845702708",
+        'categories_id': 1
+    }, {
+        'id': 3,
+        'name': 'Iphone 15 pro max',
+        'price': 25000000,
+        'image': "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-naturaltitanium?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692845702708",
+        'categories_id': 1
+    }]
